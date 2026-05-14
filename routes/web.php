371bf6f2458662fdb3fceb238d347e9bed362d35
ariefprasetyo <alipoprasetyo@gmail.com>
2026-05-14 19:23:05 +0800

    tambah autentikasi dan Gate

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,44 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EquipmentController;
 use App\Http\Controllers\RentalController;
 use App\Http\Controllers\PenaltyController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::resource('equipment', EquipmentController::class)->except(['show']);
-Route::resource('rentals', RentalController::class)->except(['edit', 'update']);
-Route::resource('penalties', PenaltyController::class)->only(['index', 'create', 'store', 'destroy']);
+// 1. HALAMAN UTAMA
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// 2. RUTE AUTHENTICATION (Sama seperti struktur proyek kemarin)
+Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
+Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/register', [AuthController::class, 'store'])->name('auth.store');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.authenticate');
+Route::delete('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+// 3. RUTE TERPROTEKSI (Grup Middleware Auth)
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/equipment', [EquipmentController::class, 'index'])->name('equipment.index');
+    Route::get('/equipment/create', [EquipmentController::class, 'create'])->name('equipment.create');
+    Route::post('/equipment', [EquipmentController::class, 'store'])->name('equipment.store');
+    Route::get('/equipment/{equipment}/edit', [EquipmentController::class, 'edit'])->name('equipment.edit');
+    Route::put('/equipment/{equipment}', [EquipmentController::class, 'update'])->name('equipment.update');
+    Route::delete('/equipment/{equipment}', [EquipmentController::class, 'destroy'])->name('equipment.destroy');
+
+    Route::get('/rentals', [RentalController::class, 'index'])->name('rentals.index');
+    Route::get('/rentals/create', [RentalController::class, 'create'])->name('rentals.create');
+    Route::post('/rentals', [RentalController::class, 'store'])->name('rentals.store');
+    Route::get('/rentals/{rental}', [RentalController::class, 'show'])->name('rentals.show');
+    Route::delete('/rentals/{rental}', [RentalController::class, 'destroy'])->name('rentals.destroy');
+
+    Route::get('/penalties', [PenaltyController::class, 'index'])->name('penalties.index');
+    Route::get('/penalties/create', [PenaltyController::class, 'create'])->name('penalties.create');
+    Route::post('/penalties', [PenaltyController::class, 'store'])->name('penalties.store');
+    Route::delete('/penalties/{penalty}', [PenaltyController::class, 'destroy'])->name('penalties.destroy');
+
+});
